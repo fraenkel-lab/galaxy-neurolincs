2b2e13d23a16b766af53f27620a91222351bb4b8
Alexander Lenail <alexander.lenail@tufts.edu>
2016-11-03 11:05:17 -0700

    Update launch-galaxy.sh

--- launch-galaxy.sh
+++ launch-galaxy.sh
@@ -15,3 +15,7 @@ docker run -d \
 
 # for testing (and to keep data from persisting) this command is sufficient:
 # docker run -d -p 80:80 -p 9002:9002 alenail/galaxy-neurolincs
+
+# for testing with globus integration: 
+# docker run -d -p 80:80  -p 21:21 -p 8800:8800 -p 9002:9002 -p 2811:2811 -p 2223:2223 -p 7512:7512 -p 50000-51000:50000-51000 alenail/galaxy-neurolincs
+
