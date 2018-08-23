1c0307f0fd9a2725d4f3ce6e6f3ecfcd4749187a
Alexander Lenail <zfrenchee@gmail.com>
2018-08-23 13:11:42 -0400

    update CDR script

--- scripts/cdr.py
+++ scripts/cdr.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     neurolincs_genomics        = all_subfiles("/pool/data/globus/neurolincs_genomics/")
     neurolincs_proteomics      = all_subfiles("/pool/data/globus/neurolincs_proteomics/")
     neurolincs_transcriptomics = all_subfiles("/pool/data/globus/neurolincs_transcriptomics/")
-    other_genomics             = all_subfiles("/pool/data/globus/other_genomics/")
+    # other_genomics             = all_subfiles("/pool/data/globus/other_genomics/")
 
-    files = aals_epigenomics + aals_genomics + aals_proteomics + aals_transcriptomics + neurolincs_epigenomics + neurolincs_genomics + neurolincs_proteomics + neurolincs_transcriptomics + other_genomics
+    files = aals_epigenomics + aals_genomics + aals_proteomics + aals_transcriptomics + neurolincs_epigenomics + neurolincs_genomics + neurolincs_proteomics + neurolincs_transcriptomics #+ other_genomics
 
     [print(file) for file in files]
 
