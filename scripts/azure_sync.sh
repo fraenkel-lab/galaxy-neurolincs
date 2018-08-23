81c062fe5f01649c8b33c05fcf6b3d6b7a1783dc
Alex Lenail <alex@lenail.org>
2018-08-23 13:01:47 -0400

    remote changes......?

---
+++ scripts/azure_sync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VARS=('neurolincs_genomics', 'neurolincs_epigenomics', 'neurolincs_proteomics', 'neurolincs_transcriptomics', 'aals_genomics', 'aals_epigenomics', 'aals_proteomics', 'aals_transcriptomics', 'other_genomics')
+
+for VAR in "${VARS[@]}"
+do
+	echo /pool/data/globus/$VAR
+
+
+	# azcopy --source /pool/data/globus/$VAR --destination https://thecdr.blob.core.windows.net/globus/$VAR --dest-key VfL34sl9bJ/zJ2J55NnHAJDt7me3va65REBl5YKYBh2Nzaz9KIJ5V8j1fPxUFc2UsGozrRPTHcDUd/ED1nA6/w== --recursive --exclude-older --exclude-newer
+done
+
+
