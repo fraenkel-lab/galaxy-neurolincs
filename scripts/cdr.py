f6b3db6c9b3f9a17c1f7a8d2d8a7ff8e379d5436
Alexander Lenail <zfrenchee@gmail.com>
2018-08-24 19:39:04 -0400

    updates to CDR.py script

--- scripts/cdr.py
+++ scripts/cdr.py
@@ -4,19 +4,21 @@ def flatten(list_of_lists): return [item for sublist in list_of_lists for item i
 
 def all_subfiles(path): return flatten([[os.path.join(p, file) for file in files] for p, subdirs, files in os.walk(path) if len(files)])
 
+def all_genomics_subfiles(path): return [file for file in all_subfiles(path) if "Sample_" in file]
+
 if __name__ == '__main__':
 
     aals_epigenomics           = all_subfiles("/pool/data/globus/aals_epigenomics/")
-    aals_genomics              = all_subfiles("/pool/data/globus/aals_genomics/")
+    aals_genomics              = all_genomics_subfiles("/pool/data/globus/aals_genomics/")
     aals_proteomics            = all_subfiles("/pool/data/globus/aals_proteomics/")
     aals_transcriptomics       = all_subfiles("/pool/data/globus/aals_transcriptomics/")
-    neurolincs_epigenomics     = all_subfiles("/pool/data/globus/neurolincs_epigenomics/")
+    neurolincs_epigenomics     = all_genomics_subfiles("/pool/data/globus/neurolincs_epigenomics/")
     neurolincs_genomics        = all_subfiles("/pool/data/globus/neurolincs_genomics/")
     neurolincs_proteomics      = all_subfiles("/pool/data/globus/neurolincs_proteomics/")
     neurolincs_transcriptomics = all_subfiles("/pool/data/globus/neurolincs_transcriptomics/")
-    # other_genomics             = all_subfiles("/pool/data/globus/other_genomics/")
+    # other_genomics             = all_genomics_subfiles("/pool/data/globus/other_genomics/")
 
     files = aals_epigenomics + aals_genomics + aals_proteomics + aals_transcriptomics + neurolincs_epigenomics + neurolincs_genomics + neurolincs_proteomics + neurolincs_transcriptomics #+ other_genomics
 
-    [print(file) for file in files if "level" in file]
+    map(print,files)
 
