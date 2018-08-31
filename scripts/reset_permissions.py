4c9a2db92d89e6f8f35a29a1e307efe7fbe9b48e
Alexander Lenail <zfrenchee@gmail.com>
2018-08-31 12:27:57 -0400

    oops

--- scripts/reset_permissions.py
+++ scripts/reset_permissions.py
@@ -71,9 +71,9 @@ def reset_file_permissions():
             current_owner = pwd.getpwuid(current_permissions.st_uid).pw_name
             current_group = grp.getgrgid(current_permissions.st_gid).gr_name
             if current_owner != new_owner or current_group != new_group:
-                current = "["+current_owner:" :: "+current_group+"]"
-                new = "["(new_owner if new_owner else current_owner)+" :: "+(new_group if new_group else current_group)"]"
-                print(permissions_change.format(current, new, subdir))
+                current = "["+current_owner+" :: "+current_group+"]"
+                new = "["+(new_owner if new_owner else current_owner)+" :: "+(new_group if new_group else current_group)+"]"
+                print(permissions_change.format(current, new, subdir.strip('/pool/data/globus')))
 
             if new_owner: shutil.chown(subdir, user=new_owner)
             if new_group: shutil.chown(subdir, group=new_group)
