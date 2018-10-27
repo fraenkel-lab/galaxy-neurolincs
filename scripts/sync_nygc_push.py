debbbe1d740b8d95287b01d07f03e8aa0a33aee2
Alexander Lenail <zfrenchee@gmail.com>
2018-10-27 19:47:42 -0400

    tabs -> spaces

--- scripts/sync_nygc_push.py
+++ scripts/sync_nygc_push.py
@@ -6,21 +6,21 @@ def flatten(list_of_lists): return [item for sublist in list_of_lists for item i
 
 def removeEmptyFolders(path, removeRoot=True):
 
-	if not os.path.isdir(path):
-		return
+    if not os.path.isdir(path):
+        return
 
-	# remove empty subfolders
-	files = os.listdir(path)
-	if len(files):
-		for f in files:
-			fullpath = os.path.join(path, f)
-			if os.path.isdir(fullpath):
-				removeEmptyFolders(fullpath)
+    # remove empty subfolders
+    files = os.listdir(path)
+    if len(files):
+        for f in files:
+            fullpath = os.path.join(path, f)
+            if os.path.isdir(fullpath):
+                removeEmptyFolders(fullpath)
 
-	# if folder empty, delete it
-	files = os.listdir(path)
-	if len(files) == 0 and removeRoot:
-		os.rmdir(path)
+    # if folder empty, delete it
+    files = os.listdir(path)
+    if len(files) == 0 and removeRoot:
+        os.rmdir(path)
 
 # python3 ~/galaxy-neurolincs/scripts/sync_nygc_push.py
 
@@ -29,29 +29,29 @@ def removeEmptyFolders(path, removeRoot=True):
 if __name__ == '__main__':
 
     srcdir = Path('/pool/data/globus/PUSHED_FROM_NYGC')
-	targetdir = Path('/pool/data/globus/other_genomics')
+    targetdir = Path('/pool/data/globus/other_genomics')
 
     new_files = [f.relative_to(srcdir) for f in srcdir.glob('**/*.*') if f.is_file()]
     old_files = [f.relative_to(targetdir) for f in targetdir.glob('**/*.*') if f.is_file()]
 
-	# Handle new files
-	all_new_files = list(set(new_files) - set(old_files))
+    # Handle new files
+    all_new_files = list(set(new_files) - set(old_files))
 
-	for file in all_new_files:
-		(srcdir / file).rename(targetdir / file)
+    for file in all_new_files:
+        (srcdir / file).rename(targetdir / file)
 
-	# Handle updated files
-	all_updated_files = list(set(all_pushed_files) & set(all_local_files))
+    # Handle updated files
+    all_updated_files = list(set(all_pushed_files) & set(all_local_files))
 
-	for file in all_updated_files:
+    for file in all_updated_files:
 
-		if (srcdir / file).stat().st_mtime > (targetdir / file).stat().st_mtime:
+        if (srcdir / file).stat().st_mtime > (targetdir / file).stat().st_mtime:
 
-			(srcdir / file).rename(targetdir / file)
+            (srcdir / file).rename(targetdir / file)
 
-		else:
+        else:
 
-			print("Files which we seem to have more recent copies of in "+srcdir+":")
-			print(file)
+            print("Files which we seem to have more recent copies of in "+srcdir+":")
+            print(file)
 
-	removeEmptyFolders(srcdir)
+    removeEmptyFolders(srcdir)
