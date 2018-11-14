110675a60cefb1d3ddced6174db97da5d54db111
Alexander Lenail <zfrenchee@gmail.com>
2018-11-14 10:46:36 -0500

    Split off LBC

--- scripts/azure_sync.sh
+++ scripts/azure_sync.sh
@@ -6,6 +6,6 @@ for VAR in "${VARS[@]}"
 do
         echo /pool/data/globus/$VAR
 
-        azcopy --source /pool/data/globus/$VAR --destination https://thecdr.blob.core.windows.net/globus/$VAR --dest-key VfL34sl9bJ/zJ2J55NnHAJDt7me3va65REBl5YKYBh2Nzaz9KIJ5V8j1fPxUFc2UsGozrRPTHcDUd/ED1nA6/w== --recursive --exclude-older --exclude-newer
+        azcopy sync --source /pool/data/globus/$VAR --destination https://thecdr.blob.core.windows.net/globus/$VAR --dest-key VfL34sl9bJ/zJ2J55NnHAJDt7me3va65REBl5YKYBh2Nzaz9KIJ5V8j1fPxUFc2UsGozrRPTHcDUd/ED1nA6/w== --recursive --exclude-older --exclude-newer
 done
 
--- scripts/cdr.py
+++ scripts/cdr.py
@@ -8,17 +8,23 @@ def all_genomics_subfiles(path): return [file for file in all_subfiles(path) if 
 
 if __name__ == '__main__':
 
-    aals_epigenomics           = all_subfiles("/pool/data/globus/aals_epigenomics/")
     aals_genomics              = all_genomics_subfiles("/pool/data/globus/aals_genomics/")
+    aals_epigenomics           = all_subfiles("/pool/data/globus/aals_epigenomics/")
     aals_proteomics            = all_subfiles("/pool/data/globus/aals_proteomics/")
     aals_transcriptomics       = all_subfiles("/pool/data/globus/aals_transcriptomics/")
-    neurolincs_epigenomics     = all_subfiles("/pool/data/globus/neurolincs_epigenomics/")
+
     neurolincs_genomics        = all_genomics_subfiles("/pool/data/globus/neurolincs_genomics/")
+    neurolincs_epigenomics     = all_subfiles("/pool/data/globus/neurolincs_epigenomics/")
     neurolincs_proteomics      = all_subfiles("/pool/data/globus/neurolincs_proteomics/")
     neurolincs_transcriptomics = all_subfiles("/pool/data/globus/neurolincs_transcriptomics/")
+
+    lbc_epigenomics            = all_subfiles("/pool/data/globus/lbc_epigenomics/")
+    lbc_proteomics             = all_subfiles("/pool/data/globus/lbc_proteomics/")
+    lbc_transcriptomics        = all_subfiles("/pool/data/globus/lbc_transcriptomics/")
+
     # other_genomics             = all_genomics_subfiles("/pool/data/globus/other_genomics/")
 
-    files = aals_epigenomics + aals_genomics + aals_proteomics + aals_transcriptomics + neurolincs_epigenomics + neurolincs_genomics + neurolincs_proteomics + neurolincs_transcriptomics #+ other_genomics
+    files = aals_epigenomics + aals_genomics + aals_proteomics + aals_transcriptomics + neurolincs_epigenomics + neurolincs_genomics + neurolincs_proteomics + neurolincs_transcriptomics + lbc_epigenomic + lbc_proteomic + lbc_transcriptomics #+ other_genomics
 
     [print(file) for file in files]
 
