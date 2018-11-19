1f015131dc072955ee459dac0219c19351aa5076
Alexander Lenail <zfrenchee@gmail.com>
2018-11-18 22:57:00 -0500

    Update azure_sync.sh

--- scripts/azure_sync.sh
+++ scripts/azure_sync.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 VARS=('genomics', 'genomics_other', 'epigenomics', 'proteomics', 'transcriptomics')
+SAS='?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2018-12-04T11:55:38Z&st=2018-11-19T03:55:38Z&spr=https&sig=caICNa0ugfSixq4%2B1TkM2wSMMKiQNKu6SGr50loIa9I%3D'
 
 for VAR in "${VARS[@]}"
 do
         echo /pool/data/globus/$VAR
 
-        azcopy sync --source /pool/data/globus/$VAR --destination https://thecdr.blob.core.windows.net/globus/$VAR --dest-key VfL34sl9bJ/zJ2J55NnHAJDt7me3va65REBl5YKYBh2Nzaz9KIJ5V8j1fPxUFc2UsGozrRPTHcDUd/ED1nA6/w==
+        azcopy sync /pool/data/globus/$VAR https://thecdr.blob.core.windows.net/globus/$VAR?$SAS --recursive=true
 done
 
